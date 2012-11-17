43fd1bb31b61e558b2f40dfd2ff96fd69defd6e4
Luke McNeice <l.mcneice@kainos.com>
2012-11-17 14:25:50 +0000

    Tidied Project folders and added HeroParser Class

---
+++ Dota2App/Dota2App/HeroParser.h
@@ -0,0 +1,13 @@
+//
+//  HeroParser.h
+//  Dota2App
+//
+//  Created by Luke McNeice on 17/11/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HeroParser : NSObject
+
+@end
---
+++ Dota2App/Dota2App/HeroParser.m
@@ -0,0 +1,13 @@
+//
+//  HeroParser.m
+//  Dota2App
+//
+//  Created by Luke McNeice on 17/11/2012.
+//
+//
+
+#import "HeroParser.h"
+
+@implementation HeroParser
+
+@end
