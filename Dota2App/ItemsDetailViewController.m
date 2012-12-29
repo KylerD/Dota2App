8f6ccd5e059b09888281c442b0f8d7def49f42e0
Kyle Davidson <k.davidson@kainos.com>
2012-12-29 12:59:13 +0000

    Item detail creation - In progress

---
+++ Dota2App/ItemsDetailViewController.m
@@ -0,0 +1,39 @@
+//
+//  ItemsDetailViewController.m
+//  Dota2App
+//
+//  Created by Kyle Davidson on 29/12/2012.
+//
+//
+
+#import "ItemsDetailViewController.h"
+
+@interface ItemsDetailViewController ()
+
+@end
+
+@implementation ItemsDetailViewController
+@synthesize item;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
