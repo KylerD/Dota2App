396a84e1d46b1473ff93091bc62f78a2d3f5aded
Stuart McKee <stuart.mckee@kainos.com>
2013-01-02 11:17:26 +0000

    Creating a custom cell for item

---
+++ Dota2App/Dota2App/ItemCell.h
@@ -0,0 +1,13 @@
+//
+//  ItemCell.h
+//  Dota2App
+//
+//  Created by Stuart McKee on 29/12/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemCell : UITableViewCell
+
+@end
---
+++ Dota2App/Dota2App/ItemCell.m
@@ -0,0 +1,29 @@
+//
+//  ItemCell.m
+//  Dota2App
+//
+//  Created by Stuart McKee on 29/12/2012.
+//
+//
+
+#import "ItemCell.h"
+
+@implementation ItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
