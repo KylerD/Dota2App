ed3b086db0b97d6adec2e20381dfdf74a736abbe
Kyle Davidson <k.davidson@kainos.com>
2013-01-07 11:24:28 +0000

    Added feedbackVC to let users provide suggestions, maybe link to rate the app etc. Should discuss how this will be presented for ipad/iphone, preferably consistent implementation e.g. first screen of app on first launch, then doesn't show up again

---
+++ Dota2App/FeedbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedbackViewController.h
+//  Dota2App
+//
+//  Created by Kyle Davidson on 07/01/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedbackViewController : UIViewController
+
+@end
